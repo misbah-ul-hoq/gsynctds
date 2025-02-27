73d3cb1ae227c9d643f2c6f6ce8c1e66cbed93d6
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-02-27 15:14:12 +0600

    created layout

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -3,17 +3,19 @@ import Sidebar from "@/components/layout/Sidebar";
 import store from "@/redux/store";
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
-
+import { SessionProvider } from "next-auth/react";
 const StoreProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <Provider store={store}>
-      <div className={`grid lg:grid-cols-12 gap-5`}>
-        <div className={`lg:col-span-3`}>
-          <Sidebar />
+    <SessionProvider>
+      <Provider store={store}>
+        <div className={`flex gap-5 lg:grid-cols-12`}>
+          <div className={`lg:col-span-3 lg:w-[250px]`}>
+            <Sidebar />
+          </div>
+          <div className={`flex-grow lg:col-span-9`}> {children}</div>
         </div>
-        <div className={`lg:col-span-9`}> {children}</div>
-      </div>
-    </Provider>
+      </Provider>
+    </SessionProvider>
   );
 };
 
