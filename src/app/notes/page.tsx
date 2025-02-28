31b97e79baae625150f06fd339345752a90eeb90
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-02-28 12:38:34 +0600

    working on events

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const NotesPage = () => {
   const { data } = useSession();
   const [events, setEvents] = useState(null);
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
 
   console.log(events);
   useEffect(() => {
@@ -17,7 +21,26 @@ const NotesPage = () => {
     };
     fetchEvents();
   }, []);
-  return <div>NotesPage</div>;
+  return (
+    <div className="my-4">
+      <div className="mb-3">
+        <button className="btn btn-outline btn-primary" onClick={() => {}}>
+          Add Event
+        </button>
+      </div>
+
+      <div className="">
+        <div>
+          <p className="">Start</p>
+          <DatePicker
+            selected={start}
+            onChange={(date) => setStart(date)}
+            showTimeSelect
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotesPage;
