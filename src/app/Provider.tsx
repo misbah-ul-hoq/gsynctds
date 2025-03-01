4b1f1839c61092d1526692f03ed915cc0a9e2680
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-03-01 19:31:53 +0600

    created sidebar hiding logic

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,15 +1,22 @@
 "use client";
 import Sidebar from "@/components/layout/Sidebar";
 import store from "@/redux/store";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 const StoreProvider = ({ children }: { children: ReactNode }) => {
+  const [authToken, setAuthToken] = useState<undefined | null | string>();
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) setAuthToken(token);
+  }, []);
   return (
     <SessionProvider>
       <Provider store={store}>
         <div className={`flex gap-5 lg:grid-cols-12`}>
-          <div className={`lg:col-span-3 lg:w-[250px]`}>
+          <div
+            className={`lg:col-span-3 lg:w-[250px] ${!authToken && "hidden"}`}
+          >
             <Sidebar />
           </div>
           <div className={`flex-grow lg:col-span-9`}> {children}</div>
